ef7dd549b4f78b4efd0b27e48009374ecfbeb1db
dagoberto <berto35572@gmail.com>
2021-06-21 14:07:38 -0600

    added selectable spinner functional

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.animals.controller;
 
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,14 +28,26 @@ public class MainActivity extends AppCompatActivity {
   private WebView contentView;
   private Spinner animalSelector;
 
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     contentView = findViewById(R.id.content_view);
-    setWebView();
     animalSelector = findViewById(R.id.animal_selector);
+    animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Animal animal = (Animal) parent.getItemAtPosition(position);
+        contentView.loadUrl(animal.getImageURL());
+
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+      }
+    });
+    setWebView();
+
 
   }
 
@@ -68,16 +83,10 @@ public class MainActivity extends AppCompatActivity {
 
           List<Animal> animals = response.body();
           //assert animals != null;
-          String url = animals.get(0).getImageURL();
-          contentView.loadUrl(url);
-          ArrayAdapter<Animal>adapter = new ArrayAdapter<>(MainActivity.this,
-              R.layout.item_animal_spinner, animals );
-
-          runOnUiThread(() -> {
-
-            animalSelector.setAdapter(adapter);
-
-          });
+          ArrayAdapter<Animal> adapter = new ArrayAdapter<>(MainActivity.this,
+              R.layout.item_animal_spinner, animals);
+          adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+          runOnUiThread(() -> animalSelector.setAdapter(adapter));
 
         } else {
           Log.e(getClass().getName(), response.message());
